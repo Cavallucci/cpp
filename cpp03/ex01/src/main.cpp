e19f0da4baee8101e8c1df181fcdeaf821405a90
Laura CAVALLUCCI <lcavallu@e2r3p16.clusters.42paris.fr>
2022-04-10 17:33:35 +0200

    finis ex00

--- cpp03/ex01/src/main.cpp
+++ cpp03/ex01/src/main.cpp
@@ -3,9 +3,14 @@
 
 int	main(void)
 {
-	ScavTrap bob("Bob");
+	ScavTrap bob("Michel");
 
-	bob.attack("Robert");
+	std::cout << std::endl;
+	bob.attack("Guy");
+	std::cout << std::endl;
 	bob.takeDamage(5);
+	std::cout << std::endl;
 	bob.beRepaired(1);
+	std::cout << std::endl;
+	bob.guardGate();
 }
