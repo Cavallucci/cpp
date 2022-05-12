5f4099a92e33adfdf1f51381faa7cf02d987bb78
Laura CAVALLUCCI <lcavallu@e2r3p16.clusters.42paris.fr>
2022-05-12 18:14:58 +0200

    cases fait

--- cpp06/ex00/src/Conversion.cpp
+++ cpp06/ex00/src/Conversion.cpp
@@ -3,8 +3,8 @@
 Conversion::Conversion(std::string a) : 
 _char('\0'), _int(0), _float(0), _double(0), _type(INT)
 {
-	checkType(a);
 	std::cout << "Conversion constructor" << std::endl;
+	checkType(a);
 }
 
 Conversion::Conversion(void) :
@@ -36,16 +36,37 @@ Conversion	&Conversion::operator=(Conversion const &rhs)
 
 void	Conversion::printValue(void)
 {
-	
+	switch (_type)
+	{
+		case INT:
+			std::cout << "int" << std::endl;
+			break;
+		case FLOAT:
+			std::cout << "float" << std::endl;
+			break;
+		case DOUBLE:
+			std::cout << "double" << std::endl;
+			break;
+		case CHAR:
+			std::cout << "char" << std::endl;
+			break;
+		case ERROR:
+			std::cout << "error" << std::endl;
+			break;
+	}
 }
 
 void	Conversion::checkType(std::string a)
 {
+	int i = 0;
+	
 	if (isdigit(a[0]))
 	{
-		
-		std::cout << "chiffre" << std::endl;
+		while (a[i])
+			i++;
+		if (a[i - 1] == 'f')
+			this->_type = FLOAT;	
 	}
-	std::cout << _type << std::endl;	
-	std::cout << a << std::endl;	
+	else if (isalpha(a[0]))
+		_type = CHAR;
 }
